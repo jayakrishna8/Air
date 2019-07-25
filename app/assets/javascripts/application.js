6f0366bb8603e136ec670c1777c94b6ea2c9a380
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-25 23:07:12 +0530

    First commit and integrated authentication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require rails-ujs
 //= require jquery
 //= require bootstrap-sprockets
+//= require toastr
 //= require activestorage
 //= require turbolinks
 //= require_tree .
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   enable_starttls_auto: true,
+  #   authentication: 'plain',
+  #   user_name: 'example@gmail.com',
+  #   password: 'example'
+  # }
 end
