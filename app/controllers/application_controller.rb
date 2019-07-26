55efda62603fa892a6d9eb3e6632bdf4e9c215cb
jayakrishna8 <jayakrishna.nalli@gmail.com>
2019-07-26 23:49:01 +0530

    Added new navbar and phone number updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parametes, if: :devise_controller?
+  before_action :configure_permitted_paramters, if: :devise_controller?
 
   protected
+    def configure_permitted_paramters
 
-  def configure_permitted_parametes
-  	devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-  	devise_parameter_sanitizer.permit(:account_update, keys: [:fullname])
-  end
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
+
+        devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, 
+        :phone_number, :description, :email, :password])
+
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_194424) do
+ActiveRecord::Schema.define(version: 2019_07_26_164821) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,9 @@ ActiveRecord::Schema.define(version: 2019_06_11_194424) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.string "unconfirmed_email"
+    t.string "phone_number"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
